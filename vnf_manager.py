b1ce9acb6e3adff9262c31b49b0e5f66c2fb1368
andresmaca <mhandres@unicauca.edu.co>
2019-09-02 23:19:46 -0500

    se

--- vnf_manager.py
+++ vnf_manager.py
@@ -16,29 +16,30 @@ def get_osm_authentication_token():
     response_parsed = response.content.split()
     return response_parsed[2].decode("utf-8")
 
-def get_ns_list():
+def get_ns_list(auth_token):
     url = "https://localhost:9999/osm/nslcm/v1/ns_instances"
     payload = ""
     headers = {
         'Content-Type': "application/",
-        'Authorization': "Bearer "+self.auth_token,
+        'Authorization': "Bearer "+ auth_token,
         'cache-control': "no-cache",
         }
     return requests.request("GET", url, data=payload, headers=headers, verify=False)
 
-def get_vnf_list(ns_id):
+def get_vnf_list(ns_id, auth_token):
     url = "https://localhost:9999/osm/nslcm/v1/vnf_instances?nsr-id-ref="+ns_id
     payload = ""
     headers = {
         'Content-Type': "application/",
-        'Authorization': "Bearer "+self.auth_token,
+        'Authorization': "Bearer "+auth_token,
         'cache-control': "no-cache",
         }
     return requests.request("GET", url, data=payload, headers=headers, verify=False)
 
 
 if __name__ == "__main__":
-    print(get_ns_list())
+    auth_token = get_osm_authentication_token()
+    print(get_ns_list(auth_token))
 
     """
     osm_auth_token = get_osm_authentication_token()
