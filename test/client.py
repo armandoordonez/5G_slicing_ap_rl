5cd9871c192da391a2213ffd5984444bf44b362b
andresmaca <mhandres@unicauca.edu.co>
2019-11-01 22:18:57 -0500

    working...

--- test/client.py
+++ test/client.py
@@ -7,21 +7,47 @@ class MultipleRequest():
         self.target_ip = "http://"+target_ip + ":" + target_port
         print("target ip: {}".format(self.target_ip))    
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.start())
-
-
+        loop.run_until_complete(self.set_clients())
 
     async def start(self):
         while True:
             print("starting new loop")
             gather_vector = []
-            for _ in range(55): # 55 
-                gather_vector.append(self.download(self.target_ip))
+
+            for counter in range(1): # 55 
+
+                gather_vector.append(self.main(counter = counter))
             await asyncio.gather(*gather_vector)
             await asyncio.sleep(3)
             
             #gc.collect()
             #await asyncio.gather(*gather_vector)
+    async def set_clients(self):
+        print("starting new loop")
+        gather_vector = []
+        for counter in range(100): # 55 
+            gather_vector.append(self.client(counter = counter))
+        await asyncio.gather(*gather_vector)
+        print("ending loop...")
+        await asyncio.sleep(3)
+    async def client(self, counter):
+        async with aiohttp.ClientSession() as session:
+            while True:
+                print("#{} requesting... ".format(counter))
+                html = await self.fetch(session, self.target_ip)
+                print(html)
+                #await asyncio.sleep(2)
+
+    async def fetch(self, session, url):
+        async with session.get(url) as response:
+            return await response.text()
+
+    async def main(self, counter):
+        async with aiohttp.ClientSession() as session:
+            print("request #{}".format(counter))
+            html = await self.fetch(session, self.target_ip)
+            #print()
+        await self.main(counter)
 
     async def download(self, url):
         print("download...")
--- test/video_asyncio.py
+++ test/video_asyncio.py
@@ -1,5 +1,15 @@
 from aiohttp import web
+routes = web.RouteTableDef()
+
+@routes.get('/calculate')
+async def calculate(request):
+    print("holi")
+    number = 12
+    for _ in range(number):
+        number = number * number
+    return web.Response(text="Calculated..")
 app = web.Application()
 app.router.add_static('/download','/home', show_index= True)
+app.add_routes(routes)
 web.run_app(app)
 
